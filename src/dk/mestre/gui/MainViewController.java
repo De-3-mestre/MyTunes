9c2091d99682b66c12429459f8d0071eff61faec
lazynormz <skupi1234@gmail.com>
2020-12-01 13:51:01 +0100

    Added all injected methods

--- src/dk/mestre/gui/MainViewController.java
+++ src/dk/mestre/gui/MainViewController.java
@@ -1,4 +1,48 @@
 package dk.mestre.gui;
 
+import javafx.event.ActionEvent;
+
 public class MainViewController {
+    public void handleNewPlaylist() {
+    }
+
+    public void handleEditPlaylist() {
+
+    }
+
+    public void handleDeletePlaylist() {
+    }
+
+    public void handleMoveSongDown() {
+    }
+
+    public void handleMoveSongUp() {
+    }
+
+    public void handleDeleteSongPlaylist() {
+    }
+
+    public void handleNewSong() {
+    }
+
+    public void handleEditSong() {
+    }
+
+    public void handleDeleteSong() {
+    }
+
+    public void handlePreviousSong() {
+    }
+
+    public void handlePlaySong() {
+    }
+
+    public void handleNextSong() {
+    }
+
+    public void handleFilterSongs() {
+    }
+
+    public void handleMoveSongToPlaylist() {
+    }
 }
